93c09c3fa860d560fbf89b8a70e5bc339d781a97
Sahil Agashe <sahil@scrambleapps.gmail.com>
2024-01-06 22:35:11 +0530

    Initial Commit

---
+++ CustomDropDownMenu/CustomDropDownMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDropDownMenuApp.swift
+//  CustomDropDownMenu
+//
+//  Created by SAHIL AMRUT AGASHE on 06/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomDropDownMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
